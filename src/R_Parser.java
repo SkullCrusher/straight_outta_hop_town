b47935494e2b29d7a5cae1936024031fc5cd182e
HendJordan <jdh423@gmail.com>
2016-03-20 17:16:39 -0700

    updated parser

--- src/R_Parser.java
+++ src/R_Parser.java
@@ -2,24 +2,106 @@ package package_1;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import gov.nasa.worldwind.WorldWindow;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.Position;
+
 import org.w3c.dom.Node;
-import java.io.File;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 
 public class R_Parser {
 	
-	public void Parse(){
-
-
-		    try {
-
-			File fXmlFile = new File("C:/_/straight_outta_hop_town/wa.xml");
+	public void Parse(String areaCode){		
+		
+		/*******areaCodes****
+		 * All - 'us'
+		 * Alabama - 'al'
+		 * Alaska - 'ak'
+		 * America Samoa - 'as'
+		 * Arizona - 'az'
+		 * Arkansas - 'ar'
+		 * California - 'ca'
+		 * Colorado - 'co'
+		 * Connecticut - ct
+		 * Delaware - 'de'
+		 * District of Columbia - 'dc'
+		 * Florida - 'fl'
+		 * Georgia - 'ga'
+		 * Guam - 'gu'
+		 * Hawaii - 'hi'
+		 * Idaho - 'id'
+		 * Illinois - 'il'
+		 * Indiana - 'in'
+		 * Iowa - 'ia'
+		 * Kansas - 'ks'
+		 * Kentucky - 'ky'
+		 * Louisiana - 'la'
+		 * Maine - 'me'
+		 * Marianas - 'mp'
+		 * Maryland - 'md'
+		 * Massachusetts - 'ma'
+		 * Michigan - 'mi'
+		 * Midway Island - 'um'
+		 * Minnesota - 'mn'
+		 * Mississippi - 'ms'
+		 * Missouri - 'mo'
+		 * Montana - 'mt'
+		 * Nevada - 'nv'
+		 * Nebraska - 'ne'
+		 * New Hampshire - 'nh'
+		 * New Jersey - 'nj'
+		 * New Mexico - 'nm'
+		 * New York - 'ny'
+		 * North Carolina - 'nc'
+		 * North Dakota - 'nd'
+		 * Ohio - 'oh'
+		 * Oklahoma - 'ok'
+		 * Oregon - 'or'
+		 * Pennsylvania - 'pa'
+		 * Puerto Rico - 'pr'
+		 * Rhode Island - 'ri'
+		 * South Carolina - 'sc'
+		 * South Dakota - 'sd'
+		 * Tennessee - 'tn'
+		 * Texas - 'tx'
+		 * Utah - 'ut'
+		 * Vermont - 'vt'
+		 * Virgin Islands - 'vi'
+		 * Virginia - 'va'
+		 * Washington - 'wa'
+		 * West Virginia - 'wv'
+		 * Wisconsin - 'wi'
+		 * Wyoming - 'wy'
+		*/
+			String html = request("http://alerts.weather.gov/cap/"+areaCode+".php?x=1");
+			Alert newAlert = new Alert();
+		    Plotter plotter = new Plotter();
+	//	    Position pos = new Position(null, null,0);
+			
+		//    System.out.println(html);
+		    
+			//File fXmlFile = new File("C:/_/straight_outta_hop_town/wa.xml");
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			try{
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(fXmlFile);
+			Document doc = dBuilder.parse(new InputSource(new ByteArrayInputStream(html.getBytes("utf-8"))));
 					
 			doc.getDocumentElement().normalize();
 
@@ -27,12 +109,12 @@ public class R_Parser {
 					
 			NodeList nList = doc.getElementsByTagName("entry");
 					
-			System.out.println("\n----------------------------");
+		//	System.out.println("\n----------------------------");
 			
-			System.out.println(nList.getLength());
+		//	System.out.println(nList.getLength());
 
 			for (int temp = 0; temp < nList.getLength(); temp++) {
-				System.out.println("\n----------------------------");
+				//System.out.println("\n----------------------------");
 				Node nNode = nList.item(temp);
 						
 				//System.out.println("\nCurrent Element :" + nNode.getNodeName());
@@ -40,24 +122,49 @@ public class R_Parser {
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
 					Element eElement = (Element) nNode;
-
-					System.out.println("Link: " + eElement.getElementsByTagName("id").item(0).getTextContent());
-					System.out.println("summary: " + eElement.getElementsByTagName("summary").item(0).getTextContent());
-					System.out.println("Event: " + eElement.getElementsByTagName("cap:event").item(0).getTextContent());
-					System.out.println("Effective: " + eElement.getElementsByTagName("cap:effective").item(0).getTextContent());
-					System.out.println("Expires: " + eElement.getElementsByTagName("cap:expires").item(0).getTextContent());
-					System.out.println("Status: " + eElement.getElementsByTagName("cap:status").item(0).getTextContent());
 					
-					System.out.println("msgType: " + eElement.getElementsByTagName("cap:msgType").item(0).getTextContent());
-					System.out.println("Category: " + eElement.getElementsByTagName("cap:category").item(0).getTextContent());
-					System.out.println("Urgency: " + eElement.getElementsByTagName("cap:urgency").item(0).getTextContent());
-					System.out.println("Severity: " + eElement.getElementsByTagName("cap:severity").item(0).getTextContent());
-					System.out.println("Certainty: " + eElement.getElementsByTagName("cap:certainty").item(0).getTextContent());
-					System.out.println("areaDesc: " + eElement.getElementsByTagName("cap:areaDesc").item(0).getTextContent());
-					System.out.println("Status: " + eElement.getElementsByTagName("cap:status").item(0).getTextContent());
+					newAlert.setLinkUrl(eElement.getElementsByTagName("id").item(0).getTextContent());
+					newAlert.setSummary(eElement.getElementsByTagName("summary").item(0).getTextContent());
+					newAlert.setEvent(eElement.getElementsByTagName("cap:event").item(0).getTextContent());
+					newAlert.setEffTime(eElement.getElementsByTagName("cap:effective").item(0).getTextContent());
+					newAlert.setExpTime(eElement.getElementsByTagName("cap:expires").item(0).getTextContent());
+					newAlert.setStat(eElement.getElementsByTagName("cap:status").item(0).getTextContent());
+					newAlert.setMsgType(eElement.getElementsByTagName("cap:msgType").item(0).getTextContent());
+					newAlert.setCategory(eElement.getElementsByTagName("cap:category").item(0).getTextContent());
+					newAlert.setUrgency(eElement.getElementsByTagName("cap:urgency").item(0).getTextContent());
+					newAlert.setSeverity(eElement.getElementsByTagName("cap:severity").item(0).getTextContent());
+					newAlert.setCertainty(eElement.getElementsByTagName("cap:certainty").item(0).getTextContent());
+					newAlert.setAreaDesc(eElement.getElementsByTagName("cap:areaDesc").item(0).getTextContent());
+					String geoCode = (eElement.getElementsByTagName("value").item(1).getTextContent());
+					String []array = geoCode.split(" ");
+					for(int i=0; i<array.length ; i++){
+						//System.out.println(array[i]);
+						//need to get the name for the county/zone
+						String result = request("http://alerts.weather.gov/cap/wwaatmget.php?x="+array[i]+"&y=1");
+						//we want the title element
+						Document doc_2 = dBuilder.parse(new InputSource(new ByteArrayInputStream(result.getBytes("utf-8"))));	
+						doc_2.getDocumentElement().normalize();
+						String areaTitle = doc_2.getElementsByTagName("title").item(0).getTextContent();
+						//fun with strings
+						String []titlePieces = areaTitle.split(array[i]);
+						titlePieces = titlePieces[0].split("for");
+						areaTitle = titlePieces[1].substring(1);
+						areaTitle = areaTitle.substring(0, (areaTitle.length()-2));
+					//	System.out.println(areaTitle);
+						String encoded = URLEncoder.encode(areaTitle, "UTF-8");
+						String googleRequestUrl = "https://maps.googleapis.com/maps/api/geocode/xml?address="+encoded;
+						String googleResult = request(googleRequestUrl);
+				//		System.out.println(googleResult);
+						doc_2 = dBuilder.parse(new InputSource(new ByteArrayInputStream(googleResult.getBytes("utf-8"))));
+						doc_2.getDocumentElement().normalize();
+						Double lat = Double.parseDouble(doc_2.getElementsByTagName("lat").item(0).getTextContent());
+						Double lng = Double.parseDouble(doc_2.getElementsByTagName("lng").item(0).getTextContent());
+					//	Angle lat_a = new Angle();
+						Position pos = new Position(Angle.fromDegrees(lat), Angle.fromDegrees(lng), 0);
+						newAlert.pushPosition(pos);
+					}
 					
-					
-					
+					plotter.plot(newAlert);
 					
 					//System.out.println("Staff id : " + eElement.getAttribute("id"));
 					/*System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
@@ -66,11 +173,57 @@ public class R_Parser {
 					System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
 */
 				}
+			}} catch(Exception e){
+				e.printStackTrace();
 			}
-		    } catch (Exception e) {
-			e.printStackTrace();
-		    }
-		  
-	}
+	}    
 	
+	String request(String reqUrl){
+		String urlParam = "application\\x-www-form-urlencoded";
+		HttpURLConnection connection;
+	    try {
+	    //build the connection and make the request
+		//String urlString = reqUrl;
+		//urlString = urlString.replace(arg0, );
+		URL url = new URL(reqUrl);
+		connection = (HttpURLConnection)url.openConnection();
+		connection.setRequestMethod("POST");
+		connection.setRequestProperty("Content-Type", 
+				urlParam);
+		 connection.setRequestProperty("Content-Length", 
+			        Integer.toString(urlParam.getBytes().length));
+		connection.setRequestProperty("Content-Language", "en-US");
+		
+	    connection.setUseCaches(false);
+	    connection.setDoOutput(true);
+	    
+	    //send request
+	    DataOutputStream wr = new DataOutputStream (
+	            connection.getOutputStream());
+	        wr.writeBytes("http://alerts.weather.gov/cap/capatomproduct.xsl");
+	        wr.close();
+	    
+	    //get the response
+	    InputStream is = connection.getInputStream();
+	    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+	    StringBuilder response = new StringBuilder(); // or StringBuffer if not Java 5+ 
+	    String line;
+	    while((line = rd.readLine()) != null) {
+	      response.append(line);
+	      response.append('\r');
+	    }
+	    rd.close();
+	    return response.toString();
+	} catch (MalformedURLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} finally{
+		
 	}
+	    return null;
+	}
+}
+	
