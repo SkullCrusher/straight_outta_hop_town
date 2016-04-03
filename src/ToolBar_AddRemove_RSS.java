2f48f4fdcc4a820c8c2531700f23e2a98575b136
David M Harkins <boxhead.two.play@gmail.com>
2016-04-03 15:53:43 -0700

    Working on adding and remove menu

--- src/ToolBar_AddRemove_RSS.java
+++ src/ToolBar_AddRemove_RSS.java
@@ -3,7 +3,9 @@
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class ToolBar_AddRemove_RSS {
 
@@ -27,6 +29,75 @@ public class ToolBar_AddRemove_RSS {
 		
 		JFrame frame = new JFrame("Add/Remove RSS Feeds");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel controls = new JPanel();
+		
+		String[] RSS_List = {  
+		 "All - 'us'",
+		 "Alabama - 'al'",
+		 "Alaska - 'ak'",
+		 "America Samoa - 'as'",
+		 "Arizona - 'az'",
+		 "Arkansas - 'ar'",
+		 "California - 'ca'",
+		 "Colorado - 'co'",
+		 "Connecticut - ct",
+		 "Delaware - 'de'",
+		 "District of Columbia - 'dc'",
+		 "Florida - 'fl'",
+		 "Georgia - 'ga'",
+		 "Guam - 'gu'",
+		 "Hawaii - 'hi'",
+		 "Idaho - 'id'",
+		 "Illinois - 'il'",
+		 "Indiana - 'in'",
+		 "Iowa - 'ia'",
+		 "Kansas - 'ks'",
+		 "Kentucky - 'ky'",
+		 "Louisiana - 'la'",
+		 "Maine - 'me'",
+		 "Marianas - 'mp'",
+		 "Maryland - 'md'",
+		 "Massachusetts - 'ma'",
+		 "Michigan - 'mi'",
+		 "Midway Island - 'um'",
+		 "Minnesota - 'mn'",
+		 "Mississippi - 'ms'",
+		 "Missouri - 'mo'",
+		 "Montana - 'mt'",
+		 "Nevada - 'nv'",
+		 "Nebraska - 'ne'",
+		 "New Hampshire - 'nh'",
+		 "New Jersey - 'nj'",
+		 "New Mexico - 'nm'",
+		 "New York - 'ny'",
+		 "North Carolina - 'nc'",
+		 "North Dakota - 'nd'",
+		 "Ohio - 'oh'",
+		 "Oklahoma - 'ok'",
+		 "Oregon - 'or'",
+		 "Pennsylvania - 'pa'",
+		 "Puerto Rico - 'pr'",
+		 "Rhode Island - 'ri'",
+		 "South Carolina - 'sc'",
+		 "South Dakota - 'sd'",
+		 "Tennessee - 'tn'",
+		 "Texas - 'tx'",
+		 "Utah - 'ut'",
+		 "Vermont - 'vt'",
+		 "Virgin Islands - 'vi'",
+		 "Virginia - 'va'",
+		 "Washington - 'wa'",
+		 "West Virginia - 'wv'",
+		 "Wisconsin - 'wi'",
+		 "Wyoming - 'wy'"};
+		
+		
+		JComboBox Select = new JComboBox(RSS_List);
+		
+		controls.add(Select);
+		
+		frame.add(controls);
 		
 		//frame.add(P);
 		//frame.getContentPane().add(P);
