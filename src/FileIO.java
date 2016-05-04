b16c1f052b38c47589687e67526ddf500a889780
David M Harkins <boxhead.two.play@gmail.com>
2016-05-03 17:32:21 -0700

    test

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,6 +1,58 @@
 package package_1;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import package_1.Data_Manager.Feed;
 
 public class FileIO {
 
+	public static ArrayList<Feed> LoadFeeds(){
+		   FileInputStream in = null;
+		   
+		   in = new FileInputStream("input.txt");
+		        
+		   String value; 
+		   while ((value = in.read()) != -1) {
+		             
+		         }
+		     
+		         
+		         if (in != null) {
+		            in.close();
+		         }
+		       
+		
+		return null;
+	}
+	
+	public static void DumpFeeds(ArrayList<Feed> arg){
+		
+		  FileOutputStream out = null;
+		  
+		  try {
+			out = new FileOutputStream("output.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  
+		if (out != null) {
+	       try {
+					out.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	         }
+		
+		int c;
+        
+        
+        out.write(c);
+	}
+	
 }
