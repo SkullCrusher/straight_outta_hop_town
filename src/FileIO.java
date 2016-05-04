7be7f1db113e0954af002b4a42208f8ed52a3cfb
David M Harkins <boxhead.two.play@gmail.com>
2016-05-03 17:32:37 -0700

    test

--- src/FileIO.java
+++ src/FileIO.java
@@ -17,8 +17,8 @@ public class FileIO {
 		        
 		   String value; 
 		   while ((value = in.read()) != -1) {
-		             
-		         }
+			   
+		   }
 		     
 		         
 		         if (in != null) {
